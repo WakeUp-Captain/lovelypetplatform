7cce316e872a3cc489d5f620afc21c8e903b6ee0
AoGuangcheng <aoguangcheng@qq.com>
2020-08-05 15:13:34 +0800

    init

---
+++ src/main/java/com/ctgu/lovelypetplatform/LovelypetplatformApplication.java
@@ -0,0 +1,13 @@
+package com.ctgu.lovelypetplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovelypetplatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LovelypetplatformApplication.class, args);
+    }
+
+}
