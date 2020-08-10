ca8c8d8cadb5a2ada82df2a48bf003796a1f5c33
AoGuangcheng <aoguangcheng@qq.com>
2020-08-10 13:12:44 +0800

    添加跨域请求的配置文件

--- src/main/java/com/ctgu/lovelypetplatform/controller/GoodsCon.java
+++ src/main/java/com/ctgu/lovelypetplatform/controller/GoodsCon.java
@@ -5,11 +5,13 @@ import com.ctgu.lovelypetplatform.entity.Goods;
 import com.ctgu.lovelypetplatform.service.GoodsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
+//@CrossOrigin
 @RequestMapping("/goods")
 @ResponseBody
 @Controller
--- src/main/java/com/ctgu/lovelypetplatform/controller/UserCon.java
+++ src/main/java/com/ctgu/lovelypetplatform/controller/UserCon.java
@@ -5,11 +5,13 @@ import com.ctgu.lovelypetplatform.entity.User;
 import com.ctgu.lovelypetplatform.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
+//@CrossOrigin
 @ResponseBody
 @Controller
 @RequestMapping("/user")
